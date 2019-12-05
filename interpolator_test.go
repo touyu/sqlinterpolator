4a466d93ef69348ed4286a2d61311d9fde439dc9
touyu <dev.akkey0222@gmail.com>
2019-12-05 22:33:20 +0900

    Update interpolator_test.go

--- interpolator_test.go
+++ interpolator_test.go
@@ -1,11 +1,15 @@
 package sqlinterpolator
 
-import "testing"
+import (
+	"testing"
+	"time"
+)
 
 func TestInterpolate(t *testing.T) {
-	fullQuery := "select * from users where id = 1 and name <> 'jack' and is_checked = 1"
-	placeholderQuery := "select * from users where id = ? and name <> ? and is_checked = ?"
-	args := []interface{}{1, "jack", true}
+	fullQuery := "select * from users where id = 1 and name <> 'jack' and is_checked = 1 and created_at < '1997-02-22 22:22:22'"
+	placeholderQuery := "select * from users where id = ? and name <> ? and is_checked = ? and created_at < ?"
+	createdAt := time.Date(1997, 2, 22, 22, 22, 22, 0, time.UTC)
+	args := []interface{}{1, "jack", true, createdAt}
 
 	res, err := Interpolate(placeholderQuery, args)
 	if err != nil {
